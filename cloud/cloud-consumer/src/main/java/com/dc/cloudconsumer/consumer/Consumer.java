ff6993973fbbfe954327c073a00453a63083dded
dc <17611732083@163.com>
2019-07-29 18:08:01 +0800

    cloud

---
+++ cloud/cloud-consumer/src/main/java/com/dc/cloudconsumer/consumer/Consumer.java
@@ -0,0 +1,29 @@
+package com.dc.cloudconsumer.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@Slf4j
+@EnableBinding(Sink.class)
+public class Consumer {
+
+    @StreamListener(Sink.INPUT)
+    public void handle(Person person) {
+        System.out.println("Received: " + person);
+    }
+
+    public static class Person {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String toString() {
+            return this.name;
+        }
+    }
+}
