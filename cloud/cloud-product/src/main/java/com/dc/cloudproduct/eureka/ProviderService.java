50be8c810022cb4641e39439ca63d0161a163a0e
dc <17611732083@163.com>
2019-07-31 15:55:04 +0800

    more provider

--- cloud/cloud-product/src/main/java/com/dc/cloudproduct/eureka/ProviderService.java
+++ cloud/cloud-product/src/main/java/com/dc/cloudproduct/eureka/ProviderService.java
@@ -2,15 +2,22 @@ package com.dc.cloudproduct.eureka;
 
 import com.dc.api.support.CloudComponent;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 @Slf4j
 @CloudComponent(mapping = "/provider")
 public class ProviderService {
 
+    @Resource
+    private Registration registration;
+    
     @RequestMapping(value = "/getString")
     public String getString() {
         log.info("providerService");
+        log.info("getString,port={}",registration.getPort());
         return "i am a provider";
     }
 
