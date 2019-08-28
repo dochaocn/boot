3049018f2288e84c893c586d91f3488e7931a0c3
dc <17611732083@163.com>
2019-08-28 15:57:52 +0800

    diff

--- product/src/test/java/com/dc/product/other/Diff.java
+++ product/src/test/java/com/dc/product/other/Diff.java
@@ -14,14 +14,17 @@ public class Diff {
     public static void main(String[] files) throws IOException {
         long millis = System.currentTimeMillis();
 
-        String proPath = "C://Users/Dc/Desktop/diff/" + "17T10151920H00012019070351000.txt";
-        String newPath = "C://Users/Dc/Desktop/diff/" + "17N8.23.txt";
-        String outPath = "C://Users/Dc/Desktop/diff/diff17-2.txt";
+        String proPath = "C://Users/Dc/Desktop/diff/" + files[0] + ".txt";
+        String newPath = "C://Users/Dc/Desktop/diff/" + files[1] + ".txt";;
+        String outPath = "C://Users/Dc/Desktop/diff/" + files[2] + ".txt";;
         List<String> listPro = new ArrayList<>();
         List<String> listNew = new ArrayList<>();
         Map<String,List<String>> diffMap = new HashMap<>();
 
         readFile(listPro,listNew,proPath,newPath);
+
+        compareNoNum(listPro, listNew);
+
         listPro.removeAll(listNew);
         toDiffMap(listPro,listNew,diffMap);
         writeFile(diffMap,outPath);
@@ -32,17 +35,12 @@ public class Diff {
     private static void toDiffMap(List<String> listPro, List<String> listNew,Map<String,List<String>> diffMap) {
         List<String> diffList;
         String accountNo;
-        List<String> noPro = new ArrayList<>();
-        List<String> noNew = new ArrayList<>();
         for (String linePro:listPro) {
             diffList = new ArrayList<>(2);
             accountNo = linePro.substring(22,48);
-            noPro.add(accountNo);
-            boolean flag = false;
             for (String lineNew:listNew) {
                 String accountNoNew = lineNew.substring(22,48);
                 if (accountNo.equals(accountNoNew)) {
-                    noNew.add(accountNoNew);
                     // 五级分类 生产有误 以新的为准
                     if (!lineNew.substring(229,230).equals(linePro.substring(229,230)))
                         break;
@@ -64,18 +62,32 @@ public class Diff {
 //                        break;
                     diffList.add(linePro);
                     diffList.add(lineNew);
-                    flag = true;
                     break;
                 }
             }
             diffMap.put(accountNo,diffList);
         }
-        noPro.removeAll(noNew);
-        log.error(noPro.toString());
+    }
+
+    private static void compareNoNum(List<String> listPro, List<String> listNew) {
+        List<String> proNo = new ArrayList<>();
+        List<String> newNo = new ArrayList<>();
+        listPro.forEach(no -> proNo.add(no.substring(22,48)));
+        listNew.forEach(no -> newNo.add(no.substring(22,48)));
+        if (proNo.size() > newNo.size()) {
+            proNo.removeAll(newNo);
+            System.out.println("proNo 多");
+            proNo.forEach(System.out::println);
+        } else if (proNo.size() < newNo.size()){
+            newNo.removeAll(proNo);
+            System.out.println("newNo 多");
+            newNo.forEach(System.out::println);
+        } else {
+            System.out.println("proNo 与 newNo 一样");
+        }
     }
 
     private static void writeFile(Map<String,List<String>> diffMap,String outPath) throws IOException {
-
         BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(outPath));
         String flag;
         for (String accountNo:diffMap.keySet()) {
@@ -91,7 +103,6 @@ public class Diff {
     }
 
     private static void readFile(List<String> listPro,List<String> listNew,String proPath,String newPath) throws IOException {
-
         BufferedReader readerPro = new BufferedReader(new FileReader(proPath));
         BufferedReader readerNew = new BufferedReader(new FileReader(newPath));
         String line;
@@ -105,7 +116,6 @@ public class Diff {
 
 
     private static void writeFile(List<String> listDiff,String outPath) throws IOException {
-
         BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(outPath));
         for (String line:listDiff) {
             outputStream.write((line.substring(22,48)+"\n").getBytes());
