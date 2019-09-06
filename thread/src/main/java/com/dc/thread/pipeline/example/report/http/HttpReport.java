d2b00e319615ff11022633eacd7ea347bb0b4e69
dc <17611732083@163.com>
2019-09-06 18:13:14 +0800

    report

--- thread/src/main/java/com/dc/thread/pipeline/example/report/http/HttpReport.java
+++ thread/src/main/java/com/dc/thread/pipeline/example/report/http/HttpReport.java
@@ -7,15 +7,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.util.concurrent.ExecutorService;
 
 @Slf4j
 @Component
 public class HttpReport implements ReportType {
     @Resource
     private RestTemplate restTemplate;
-    @Resource
-    private ExecutorService executorService;
 
     @Override
     public boolean isType(Object object) {
@@ -24,17 +21,9 @@ public class HttpReport implements ReportType {
 
     @Override
     public Object execute(Object body, String url){
-
-        try {
-            return executorService.submit(() -> {
-                ResponseEntity<Object> response = restTemplate.postForEntity(url, body, Object.class);
-                log.info(response.toString());
-                return new ResponseMessage(response.getStatusCode(), response.getBody());
-            }).get();
-        } catch (Exception e) {
-            log.error("http请求错误", e);
-            return null;
-        }
+        ResponseEntity<Object> response = restTemplate.postForEntity(url, body, Object.class);
+        log.info(response.toString());
+        return new ResponseMessage(response.getStatusCode(), response.getBody());
     }
 
 }
